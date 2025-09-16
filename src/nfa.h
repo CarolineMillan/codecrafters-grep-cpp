27a3b19cbdec64614aa6bd94cfbbe6646192c990
carolinemillan <carolinemillan5678@gmail.com>
2025-09-16 16:45:06 +0100

    removed comments

--- src/nfa.h
+++ src/nfa.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-#include "token.h"
 #include "state.h"
 
 using std::vector;
@@ -19,22 +18,4 @@ private:
     State* start_state = nullptr;
     vector<State> states;
     State* accept_state = nullptr;
-
-    // misc
-    //std::vector<Token> tokens;
-    //std::vector<Token> concat_tokens;
-    //std::vector<Token> postfix_tokens;
-
-    // helper functions
-    //void build_from_regex(const std::string &pattern);
-/*
-    void tokenize(const std::string &pattern);
-    void parse_escaped(const char ch);
-    int parse_char_class(const std::string &pattern, int i);
-    void add_concats();
-    static bool should_concat(const Token& previous, const Token& current);
-
-    void to_postfix();
-    void construct();
-*/
 };

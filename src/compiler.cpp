a7ad71b0b439d3ec39e199c3cff5911d28cc57d1
carolinemillan <carolinemillan5678@gmail.com>
2025-09-21 19:41:26 +0100

    added Alt to compile

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -26,55 +26,83 @@ NFA Compiler::compile(vector<Token>& tokens) {
 
         // rules go here
         switch (token.kind) {
-        case Token::KIND::Literal:
+        case Token::KIND::Literal: {
+                // add transition to start state
+                start->transitions.emplace_back(token.ch, accept);
 
-            // add transition to start state
-            start->transitions.emplace_back(token.ch, accept);
+                // add fragment to fragments stack
+                fragments.push(NFAFragment(start, accept));
 
-            // add fragment to fragments stack
-            fragments.push(NFAFragment(start, accept));
-
-            break;
-        case Token::KIND::CharClass:
-
-            // add transition to start state for each char
-            for (int c=0; c<256; c++) {
-                if (token.bitmap[c]) {
-                    start->transitions.emplace_back(static_cast<unsigned char>(c), accept);
-                }
+                break;
             }
+        case Token::KIND::CharClass: {
+                // add transition to start state for each char
+                for (int c=0; c<256; c++) {
+                    if (token.bitmap[c]) {
+                        start->transitions.emplace_back(static_cast<unsigned char>(c), accept);
+                    }
+                }
 
-            // add fragment to fragments stack
-            fragments.push(NFAFragment(start, accept));
+                // add fragment to fragments stack
+                fragments.push(NFAFragment(start, accept));
 
-            break;
-        case Token::KIND::Concat:
-            // our to_postfix means that we're in postfix notation, so the operator comes AFTER both operands
-            // so pop two fragments off the stack for our operands
-            NFAFragment b = fragments.top();
-            fragments.pop();
-            NFAFragment a = fragments.top();
-            fragments.pop();
+                break;
+            }
+        case Token::KIND::Concat: {
+                // our to_postfix means that we're in postfix notation, so the operator comes AFTER both operands
+                // so pop two fragments off the stack for our operands
 
-            // now we concat them to get a new fragment
+                // TODO should there be an error if stack is empty?
+                if (fragments.empty()) continue;
 
-            // we want an epsilon transition between a's accept state and b's start state
-            a.accept->transitions.emplace_back(EPSILON, b.start);
+                NFAFragment b = fragments.top();
+                fragments.pop();
+                NFAFragment a = fragments.top();
+                fragments.pop();
 
-            // add the new fragment to fragments
-            fragments.push(NFAFragment(a.start, b.accept));
+                // now we concat them to get a new fragment
 
+                // we want an epsilon transition between a's accept state and b's start state
+                a.accept->transitions.emplace_back(EPSILON, b.start);
+
+                // add the new fragment to fragments
+                fragments.push(NFAFragment(a.start, b.accept));
+
+                break;
+            }
+        case Token::KIND::Alt: {
+                // TODO should there be an error if stack is empty?
+                if (fragments.empty()) continue;
+
+                NFAFragment b = fragments.top();
+                fragments.pop();
+                NFAFragment a = fragments.top();
+                fragments.pop();
+
+                // add epsilon transitions from start to a.start and b.start
+                start->transitions.emplace_back(EPSILON, a.start);
+                start->transitions.emplace_back(EPSILON, b.start);
+
+                // add epsilon transitions from a.accept and b.accept to accept
+                a.accept->transitions.emplace_back(EPSILON, accept);
+                b.accept->transitions.emplace_back(EPSILON, accept);
+
+                // add fragment to fragments stack
+                fragments.push(NFAFragment(start, accept));
+
+                break;
+            }
+        case Token::KIND::Star: {
+                break;
+            }
+        case Token::KIND::Question: {
+                break;
+            }
+        case Token::KIND::Plus: {
+                break;
+            }
             break;
-        case Token::KIND::Alt:
-            break;
-        case Token::KIND::Star:
-            break;
-        case Token::KIND::Question:
-            break;
-        case Token::KIND::Plus:
-            break;
-        default:
-            break;
+        default: {break;}
         }
 
 
